93967744cd83ff0ca2de0caeb25c56494311f1a5
Luism2 <65132543+Luism2@users.noreply.github.com>
2022-02-09 19:37:16 -0600

    Modificasion de texto

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ let weather = {
       )
         .then((response) => {
           if (!response.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
+             alert("No se encontro el sitio.");
+            throw new Error("No se encontro el sitio.");
           }
           return response.json();
         })
